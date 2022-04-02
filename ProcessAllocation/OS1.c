a2837a6eb377febc3dcc1321af4cc282647364b9
Vardhan Honmane <vardhan834@gmail.com>
2022-04-02 12:59:33 +0530

    Update OS1.c

--- ProcessAllocation/OS1.c
+++ ProcessAllocation/OS1.c
@@ -1,1 +1,15 @@
 This is OS1
+Given an array of distinct elements. Find the third largest element in it.
+
+Suppose you have A[] = {1, 2, 3, 4, 5, 6, 7}, its output will be 5 because it is the 3 largest element in the array A.
+
+Example 1:
+
+Input:
+N = 5
+A[] = {2,4,1,3,5}
+
+Code Solution  - 
+  
+  
+  
